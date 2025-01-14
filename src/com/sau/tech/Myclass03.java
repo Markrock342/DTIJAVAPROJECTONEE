118e6af756b5282f095ee7ff104d35a89dd1bda0
Markrock342 <markrock342@gmail.com>
2025-01-14 17:00:13 +0700

    V.4:edit Myclass03

--- src/com/sau/tech/Myclass03.java
+++ src/com/sau/tech/Myclass03.java
@@ -1,4 +1,22 @@
 package com.sau.tech;
 
+import java.util.Scanner;
+//import java.lang.String;
+
 public class Myclass03 {
+    public static void main(String[] args) {
+        String empName;
+        double empSalary, tax10;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input employee name: ");
+        empName = sc.next();
+        System.out.print("Input employee salary: ");
+        empSalary = sc.nextDouble();
+        System.out.println("------------------------------");
+        tax10 = empSalary * 10 / 100;
+        System.out.println("TAX pay : " + tax10 + " BAHT");
+
+    }
 }
--- src/com/sau/tech/Myclass04.java
+++ src/com/sau/tech/Myclass04.java
@@ -1,4 +1,12 @@
 package com.sau.tech;
 
 public class Myclass04 {
+    public static void main(String[] atgs){
+        System.out.println("Wow Wow Wow....");
+
+
+
+
+    }
+
 }
